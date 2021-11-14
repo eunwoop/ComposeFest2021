223b15909698b753959aaf9a868c6c63280de4d8
eunwoop <eeunwoop@gmail.com>
2021-11-14 16:03:06 +0900

    13 Finishing Touches!!
    Signed-off-by: eunwoop <eeunwoop@gmail.com>

--- week 1-Jetpack Compose basics/ComposeCodelab/app/src/main/java/com/seacows/app/pill/and/composecodelab/MainActivity.kt
+++ week 1-Jetpack Compose basics/ComposeCodelab/app/src/main/java/com/seacows/app/pill/and/composecodelab/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.seacows.app.pill.and.composecodelab
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.core.*
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons.Filled
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.seacows.app.pill.and.composecodelab.ui.theme.ComposeCodelabTheme
@@ -34,21 +42,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun MyApp() {
-   var shouldShowOnBoarding by rememberSaveable { mutableStateOf(true) }
+    var shouldShowOnBoarding by rememberSaveable { mutableStateOf(true) }
     if (shouldShowOnBoarding) {
         OnBoardingScreen(
             onContinueClicked = { shouldShowOnBoarding = false })
     } else {
         GreetingList()
-    }
-}
-
-@Composable
-private fun GreetingList(names: List<String> = List(1000) { "$it" }) {
-    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
-        items(items = names) { name ->
-            Greeting(name)
-        }
     }
 }
 
@@ -72,43 +71,78 @@ fun OnBoardingScreen(onContinueClicked: () -> Unit) {
 }
 
 @Composable
+private fun GreetingList(names: List<String> = List(1000) { "$it" }) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
+            Greeting(name)
+        }
+    }
+}
+
+@Composable
 private fun Greeting(name: String) {
+    Card (
+        backgroundColor = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        CardContent(name)
+    }
+}
+
+@Composable
+private fun CardContent(name: String) {
     val expanded = remember { mutableStateOf(false) }
     val extraPadding by animateDpAsState(
         if (expanded.value) 48.dp else 0.dp,
-        animationSpec = spring(
-                dampingRatio = Spring.DampingRatioMediumBouncy,
-                stiffness = Spring.StiffnessLow,
-        )
-    )
-    val bgColor by animateColorAsState(
-        if (expanded.value)  MaterialTheme.colors.primary else MaterialTheme.colors.secondary,
-        animationSpec = tween(durationMillis = 250)
-    )
 
-    Surface(
-        color = bgColor,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
-    ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier
-                .weight(1f)
-                .padding(bottom = extraPadding.coerceAtLeast(0.dp))) {
-                Text(text = "Hello, ")
-                Text(text = name)
+        )
+
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize (
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow,
+                )
+            )
+    ){
+        Column(modifier = Modifier
+            .weight(1f)
+            .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+        ) {
+            Text(text = "Hello, ")
+            Text(text = name, style = MaterialTheme.typography.h4.copy(
+                fontWeight = FontWeight.ExtraBold
+            ))
+            if (expanded.value) {
+                Text(
+                    text = ("Composem ipsum color sit lazy, " +
+                            "padding theme elit, sed do bouncy. ").repeat(4),
+                )
             }
-            OutlinedButton(
-                onClick = { expanded.value = !expanded.value },
-                modifier = Modifier.width(120.dp)
-            ) {
-                Text(if (expanded.value) "Show less" else "Show more")
-            }
+        }
+        IconButton(
+            onClick = { expanded.value = !expanded.value },
+        ) {
+            Icon (imageVector = if (expanded.value) Filled.ExpandLess else Filled.ExpandMore,
+                contentDescription = if (expanded.value) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                })
         }
     }
 }
 
 // PREVIEW
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Preview(showBackground = true,
+    widthDp = 320,
+    heightDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark")
+@Preview(showBackground = true,
+    widthDp = 320)
 @Composable
 fun OnBoardingPreview() {
     ComposeCodelabTheme {
@@ -116,10 +150,12 @@ fun OnBoardingPreview() {
     }
 }
 
-@Preview(showBackground = true, widthDp = 320)
+@Preview(showBackground = true,
+    widthDp = 320,
+    heightDp = 320)
 @Composable
 fun DefaultPreview() {
     ComposeCodelabTheme {
-        MyApp()
+        GreetingList()
     }
 }
