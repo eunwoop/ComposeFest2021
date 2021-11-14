72562fc4d70ee58c7e84b9e9032f4a652286bd68
eunwoop <eeunwoop@gmail.com>
2021-11-14 22:48:48 +0900

    7 Create customLayout.
    Signed-off-by: eunwoop <eeunwoop@gmail.com>

--- week 2-1-Layouts in JetpackCompose/Week2App/app/src/main/java/com/seacows/app/pill/and/week2app/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/Week2App/app/src/main/java/com/seacows/app/pill/and/week2app/MainActivity.kt
@@ -1,84 +1,108 @@
 package com.seacows.app.pill.and.week2app
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.seacows.app.pill.and.week2app.ui.theme.Week2AppTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Week2AppTheme {
-                LayoutsCodelab()
+                ImageList()
             }
         }
     }
 }
 
 @Composable
-fun LayoutsCodelab() {
-    Scaffold (
-        topBar = {
-            TopAppBar (
-                title = {
-                    Text(text = "LayoutsCodelab",)
-                },
-                actions = {
-                    IconButton(onClick = {}) {
-                        Icon(Icons.Filled.Favorite, contentDescription = null)
-                    }
-                })
-        }){ innerPadding ->
-        BodyContent(Modifier.padding(innerPadding))
-    }
-}
-
-@Composable
-fun BodyContent(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.padding(8.dp)) {
-        Text(text = "Hi there!")
-        Text(text = "Thanks for going through the Codelab!")
-    }
-}
-
-@Composable
-fun PhotographerCard(modifier: Modifier = Modifier) {
-    Row(modifier
-        .padding(16.dp)
-        .clickable(onClick = {/*TODO CLICK*/ })) {
-        Surface(
-            modifier = Modifier.size(50.dp),
-            shape = CircleShape,
-            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
-        ) {
-            // IMAGE
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = rememberImagePainter(
+                data = "https://developer.android.com/images/brand/Android_Robot.png"
+            ),
+            contentDescription = "Android logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(Modifier.width(10.dp))
+        MyColumn() {
+            Text("Item #$index", style = MaterialTheme.typography.h5)
+            Text("Nice to meet you", style = MaterialTheme.typography.subtitle1)
         }
-        Column(
-            modifier = Modifier
-                .padding(start = 8.dp)
-                .align(Alignment.CenterVertically)
-        ) {
-            Text("Alfred Sisly", fontWeight = FontWeight.Bold)
-            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-                Text("3 minutes ago", style = MaterialTheme.typography.body2)
+    }
+}
+
+@Composable
+fun ImageList() {
+    val listSize = 100
+    val scrollState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+
+    Column {
+        Row (modifier = Modifier.fillMaxWidth()) {
+            Button(
+                onClick = {
+                    coroutineScope.launch {
+                        scrollState.animateScrollToItem(0)
+                    }},
+                modifier = Modifier.weight(1.0F)
+            ) {
+                Text("Scroll to the top")
+            }
+
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(listSize - 1)
+                }},
+                modifier = Modifier.weight(1.0F)) {
+                Text("Scroll to the end")
+            }
+        }
+
+        LazyColumn(state = scrollState) {
+            items(listSize) {
+                ImageListItem(it)
+            }
+        }
+    }
+}
+
+@Composable
+fun MyColumn(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        val placeables = measurables.map { measurable ->
+            measurable.measure(constraints)
+        }
+
+        var yPosition = 0
+        layout(constraints.maxWidth, constraints.minHeight) {
+            placeables.forEach { placeable ->
+                placeable.placeRelative(x = 0, y = yPosition)
+                yPosition += placeable.height
             }
         }
     }
@@ -90,7 +114,6 @@ fun PhotographerCard(modifier: Modifier = Modifier) {
 @Composable
 fun PhotographerCardPreview() {
     Week2AppTheme {
-        LayoutsCodelab()
+        ImageList()
     }
 }
-
